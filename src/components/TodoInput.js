85d03681ee7bc3c139c95aee4865357c152c8a2c
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-08-27 20:54:54 +1200

    Add content

---
+++ src/components/TodoInput.js
@@ -0,0 +1,31 @@
+import styles from "./TodoInput.module.css";
+import Swal from "sweetalert2";
+
+const TodoInput = ({ input, setInput, todoData, setTodoData }) => {
+  const addTodo = () => {
+    if (input === "") {
+      Swal.fire("請輸入代辦事項內容!", "", "error");
+    } else {
+      Swal.fire("新增成功!", "", "success");
+      setTodoData((todoData) => [
+        ...todoData,
+        { id: Date.now() + Math.random(), description: input, done: false },
+      ]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div className={styles.inputBox}>
+      <input
+        type="text"
+        placeholder="Please enter todo"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {/* {console.log(input)} */}
+      <button onClick={() => addTodo(input, setInput)}>+</button>
+    </div>
+  );
+};
+
+export default TodoInput;
