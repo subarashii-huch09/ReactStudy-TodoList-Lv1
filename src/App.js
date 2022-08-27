bdff0e56a375fa47c0d37ca08cfd875a69686bab
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-08-27 20:50:32 +1200

    Add content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import Header from "./components/Header";
+import TodoInput from "./components/TodoInput";
+import MainList from "./components/MainList";
+import Content from "./components/Content";
+import { useState } from "react";
+
+let data = [
+  {
+    id: Date.now() + Math.random(),
+    description: "把冰箱發霉的檸檬拿去丟",
+    done: true,
+  },
+  {
+    id: Date.now() + Math.random(),
+    description: "打電話叫媽媽匯款給我",
+    done: false,
+  },
+  {
+    id: Date.now() + Math.random(),
+    description: "整理電腦資料夾",
+    done: false,
+  },
+  {
+    id: Date.now() + Math.random(),
+    description: "繳電費水費瓦斯費",
+    done: false,
+  },
+  {
+    id: Date.now() + Math.random(),
+    description: "約vicky禮拜三泡溫泉",
+    done: false,
+  },
+  {
+    id: Date.now() + Math.random(),
+    description: "約ada禮拜四吃晚餐",
+    done: false,
+  },
+];
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todoData, setTodoData] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Header />
+      <TodoInput
+        input={input}
+        setInput={setInput}
+        todoData={todoData}
+        setTodoData={setTodoData}
+      />
+      <MainList>
+        <Content todoData={todoData} setTodoData={setTodoData} />
+      </MainList>
     </div>
   );
 }
